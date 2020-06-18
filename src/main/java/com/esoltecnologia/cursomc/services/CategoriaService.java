d489e12744476c55beacee47f178dbc6c66d68a1
João Lima <jmflima@gmail.com>
2020-06-18 06:02:04 -0300

    Inserindo uma nova Categoria com o POST

--- src/main/java/com/esoltecnologia/cursomc/services/CategoriaService.java
+++ src/main/java/com/esoltecnologia/cursomc/services/CategoriaService.java
@@ -15,10 +15,17 @@ public class CategoriaService {
 	@Autowired /*instancia a dependêndia automaticamente por INJEÇÃO DE DEPÊNCIA ou INVERSÃO DE CONTROLE*/
 	private CategoriaRepository repo; /*chama uma operação no objeto dados (Repository) */
 	
+	/*Pesquisando uma categoria*/
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		}
+	
+	/*Inserindo uma categoria*/
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 }
 
